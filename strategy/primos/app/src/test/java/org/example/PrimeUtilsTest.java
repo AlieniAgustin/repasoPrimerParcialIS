9472a1b9ea96f8958311ec25b3b2c14873ced2b9
AlieniAgustin <alieniagustinweb@gmail.com>
2025-10-04 18:15:08 -0300

    add parameterized tests

--- strategy/primos/app/src/test/java/org/example/PrimeUtilsTest.java
+++ strategy/primos/app/src/test/java/org/example/PrimeUtilsTest.java
@@ -1,36 +1,46 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
 
 public class PrimeUtilsTest {
 
-  @Test 
-  public void firstPrime(){
-    PrimeUtils primeUtils = new PrimeUtils(new RecursivePrimeGenerator());
-    int n = 1;
-    primeUtils.printFirstNPrimes(n);
-  }
+    //metodo que provee los generadores posibles
+    static Stream<PrimeGenerator> primeGeneratorProvider() {
+        return Stream.of(
+            new RecursivePrimeGenerator(),
+            new IterativePrimeGenerator()
+        );
+    }
 
-  @Test 
-  public void firstFourPrimes(){
-    PrimeUtils primeUtils = new PrimeUtils(new RecursivePrimeGenerator());
-    int n = 4;
-    primeUtils.printFirstNPrimes(n);
-  }
+    @ParameterizedTest
+    @MethodSource("primeGeneratorProvider")
+    public void firstPrime(PrimeGenerator generator) {
+        PrimeUtils primeUtils = new PrimeUtils(generator);
+        primeUtils.printFirstNPrimes(1);
+    }
 
-  @Test 
-  public void printNegativeNumberOfPrimes(){
-    PrimeUtils primeUtils = new PrimeUtils(new RecursivePrimeGenerator());
-    int n = -2;
-    assertThrows(IllegalArgumentException.class, () -> primeUtils.printFirstNPrimes(n));
-  }
+    @ParameterizedTest
+    @MethodSource("primeGeneratorProvider")
+    public void firstFourPrimes(PrimeGenerator generator) {
+        PrimeUtils primeUtils = new PrimeUtils(generator);
+        primeUtils.printFirstNPrimes(4);
+    }
 
-  @Test 
-  public void printNoPrime(){
-    PrimeUtils primeUtils = new PrimeUtils(new RecursivePrimeGenerator());
-    int n = 0;
-    primeUtils.printFirstNPrimes(n);
-  }
+    @ParameterizedTest
+    @MethodSource("primeGeneratorProvider")
+    public void printNegativeNumberOfPrimes(PrimeGenerator generator) {
+        PrimeUtils primeUtils = new PrimeUtils(generator);
+        assertThrows(IllegalArgumentException.class, () -> primeUtils.printFirstNPrimes(-2));
+    }
 
+    @ParameterizedTest
+    @MethodSource("primeGeneratorProvider")
+    public void printNoPrime(PrimeGenerator generator) {
+        PrimeUtils primeUtils = new PrimeUtils(generator);
+        primeUtils.printFirstNPrimes(0);
+    }
 }
